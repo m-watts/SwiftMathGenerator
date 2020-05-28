ef8250fcbb1bbb995603fe14ca9e674182600e0a
Martin Watts <stanly.watts@gmail.com>
2020-05-27 20:39:31 -0500

    Initial Commit

---
+++ mathGenerator/mathGenerator.swift
@@ -0,0 +1,11 @@
+//
+//  mathGenerator.swift
+//  mathGenerator
+//
+//  Created by Martin Watts on 27/05/2020.
+//  Copyright © 2020 Martin Watts. All rights reserved.
+//
+
+class mathGenerator {
+
+}
